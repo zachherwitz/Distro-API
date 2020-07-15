ce4ca78fdcca1ed18e827f54c18ae155613bd2bb
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-15 18:26:30 -0400

    more testing, this is a bad testing method

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -41,6 +41,8 @@ router.get('/user/:email', (req, res) => {
   User.find({email: req.params.email}, (error, foundUser) => {
     if(req.query.password === foundUser.password){
       res.json({userObject:foundUser, password:req.query.password})
+    } else {
+      res.json('something went wrong')
     }
   })
 })
