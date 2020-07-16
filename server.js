3c0bb406156a43dbdf494d3206ef302e892da08b
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-15 20:28:04 -0400

    idk

--- server.js
+++ server.js
@@ -44,11 +44,6 @@ app.use(bodyParser.json())
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", *); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 //zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach
 //                               CONTROLLERS                                //
