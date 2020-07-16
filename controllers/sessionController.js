cdc8852041be65002ccb264ee4715728f44527cb
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-15 20:41:45 -0400

    ugh removing session

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/users.js');
-const bcrypt = require('bcrypt');
-
-
-router.post('/', (req, res) => {
-    console.log(req.body)
-    User.findOne({ email:req.body.email }, (error, foundUser) => {
-        if(foundUser === null){
-            console.log(error)
-            res.json({
-                error :'Email and password combination does not match.'
-            });
-        } else {
-            if(req.body.password = foundUser.password){
-                req.session.user = foundUser;
-                res.json(foundUser)
-            } else {
-                console.log(error)
-                res.json({
-                    error :'Email and password combination does not match.'
-                });
-            }
-        }
-    });
-});
-
-router.get('/', (req, res) => {
-    console.log(req.session)
-    res.json(req.session.user);
-});
-
-router.delete('/', (req, res) => {
-    req.session.destroy(() => {
-        res.json({
-            destroyed:true
-        });
-    })
-});
-
-module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+// const User = require('../models/users.js');
+// const bcrypt = require('bcrypt');
+//
+//
+// router.post('/', (req, res) => {
+//     console.log(req.body)
+//     User.findOne({ email:req.body.email }, (error, foundUser) => {
+//         if(foundUser === null){
+//             console.log(error)
+//             res.json({
+//                 error :'Email and password combination does not match.'
+//             });
+//         } else {
+//             if(req.body.password = foundUser.password){
+//                 req.session.user = foundUser;
+//                 res.json(foundUser)
+//             } else {
+//                 console.log(error)
+//                 res.json({
+//                     error :'Email and password combination does not match.'
+//                 });
+//             }
+//         }
+//     });
+// });
+//
+// router.get('/', (req, res) => {
+//     console.log(req.session)
+//     res.json(req.session.user);
+// });
+//
+// router.delete('/', (req, res) => {
+//     req.session.destroy(() => {
+//         res.json({
+//             destroyed:true
+//         });
+//     })
+// });
+//
+// module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const cors = require('cors')
 const express = require('express');
 const methodOverride = require('method-override')
 const mongoose = require('mongoose');
-const session = require('express-session');
 
 
 //zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach
@@ -44,11 +43,6 @@ app.use(bodyParser.json())
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(cors());
-app.use(session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: false
-}));
 
 //zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach
 //                               CONTROLLERS                                //
@@ -63,9 +57,6 @@ app.use('/users', usersController);
 
 const callsheetController = require('./controllers/callsheetController.js');
 app.use('/callsheet', callsheetController);
-
-const sessionController = require('./controllers/sessionController.js');
-app.use('/admin/session', sessionController);
 
 //zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach~//~zach
 //                                LISTENER                                  //
