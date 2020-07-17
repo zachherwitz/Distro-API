00a6aeaddfeda5d032f8742786861fb6b2afc91d
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-16 23:56:20 -0400

    oops stupid typo

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -4,7 +4,7 @@ const User = require('../models/users.js');
 const bcrypt = require('bcrypt');
 
 router.post('/', (req, res) => {
-  Users.findOne({email:req.body.email}, (err, foundUser) => {
+  User.findOne({email:req.body.email}, (err, foundUser) => {
     if(foundUser === null){
       res.json({
         message: 'Email and password combination does not match'
